fb10c9150514a5a544935d0242c4838dc15ae0f0
Kalinichev Ilya <iluya.kalinichev@gmail.com>
2017-07-29 22:53:19 +0300

    improved json in the body request

--- lib/geolocator/client.rb
+++ lib/geolocator/client.rb
@@ -7,40 +7,20 @@ module Geolocator
     end
     
     def perform_request
-      json = "json={
-                    \"common\": {
-                                 \"version\": \"1.0\",
-                                 \"api_key\": \"#{@api_key}\"
-                     },
-                    \"gsm_cells\": [
-                                      {
-                                       \"countrycode\": 250,
-                                       \"operatorid\": 99,
-                                       \"cellid\": 42332,
-                                       \"lac\": 36002,
-                                       \"signal_strength\": -80,
-                                       \"age\": 5555
-                       }
-                     ],
-                    \"wifi_networks\": [
-                                         {
-                                          \"mac\": \"00-1C-F0-E4-BB-F5\",
-                                          \"signal_strength\": -88,
-                                          \"age\": 0,
-                      }
-                     ],
-                    \"ip\": {
-                            \"address_v4\": \"178.247.233.32\"
-                     }
-                    }"
+      common = { common: { version: 1.0, api_key: @api_key } }
+      gsm = { gsm_cells: { countrycode: 250, operatorid: 99, cellid: 42332, lac: 36002, signal_strength: -80, age: 5555 } }
+      wifi = { wifi_networks: { mac: '00-1C-F0-E4-BB-F5', signal_strength: -88, age: 0 } }
+      ip = { ip: { address_v4: '178.247.233.32' } }
+      
+      json = 'json='+common.to_json.to_s + gsm.to_json.to_s + wifi.to_json.to_s + ip.to_json.to_s
       
       url = 'http://api.lbs.yandex.net/geolocation'
       response = HTTParty.post(url,
-                               { :body => json,
-                                 :headers => {
-                                     :'Content-Type' => 'application/x-www-form-urlencoded',
-                                     :'Accept' => 'application/json'
-                                 } }
+                               { body: json,
+                                 headers: {
+                                     :'Content-Type' => 'application/x-www-form-urlencoded'
+                                 }
+                               }
       )
       
       JSON.parse(response.body)
