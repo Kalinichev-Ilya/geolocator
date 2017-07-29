50ac169e291504b905e34f36f7a3b7996c2658d5
Kalinichev Ilya <iluya.kalinichev@gmail.com>
2017-07-29 23:12:25 +0300

    some refactor

--- lib/geolocator/client.rb
+++ lib/geolocator/client.rb
@@ -6,13 +6,33 @@ module Geolocator
       @api_key = api_key || ENV['YANDEX_ACCESS_TOKEN']
     end
     
-    def perform_request
-      common = { common: { version: 1.0, api_key: @api_key } }
-      gsm = { gsm_cells: { countrycode: 250, operatorid: 99, cellid: 42332, lac: 36002, signal_strength: -80, age: 5555 } }
-      wifi = { wifi_networks: { mac: '00-1C-F0-E4-BB-F5', signal_strength: -88, age: 0 } }
-      ip = { ip: { address_v4: '178.247.233.32' } }
+    def perform_request(common = nil, gsm_cells = nil, wifi_networks = nil, ip = nil)
+      common = common || { common: {
+          version: 1.0,
+          api_key: @api_key }
+      }.to_json
       
-      json = 'json='+common.to_json.to_s + gsm.to_json.to_s + wifi.to_json.to_s + ip.to_json.to_s
+      gsm = gsm_cells || { gsm_cells: {
+          countrycode: 250,
+          operatorid: 99,
+          cellid: 42332,
+          lac: 36002,
+          signal_strength: -80,
+          age: 5555 }
+      }.to_json
+      
+      wifi = wifi_networks || {
+          wifi_networks: {
+              mac: '00-1C-F0-E4-BB-F5',
+              signal_strength: -88,
+              age: 0 }
+      }.to_json
+      
+      ip = ip || { ip: {
+          address_v4: '178.247.233.32' }
+      }.to_json
+      
+      json = "json=#{common}, #{gsm}, #{wifi}, #{ip}"
       
       url = 'http://api.lbs.yandex.net/geolocation'
       response = HTTParty.post(url,
