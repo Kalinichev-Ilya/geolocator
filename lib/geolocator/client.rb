8fb943c77326194a9971ffc7945d5f5d5ef56248
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-08-06 22:33:08 +0300

    problem was solved

--- lib/geolocator/client.rb
+++ lib/geolocator/client.rb
@@ -12,57 +12,33 @@ module Geolocator
       @api_key = api_key || ENV['YANDEX_ACCESS_TOKEN']
     end
     
-    
     def perform_request(common = nil, gsm_cells = [] || nil, wifi_networks = [] || nil, ip = nil)
-      # common = common || { common: {
-      #     version: 1.0,
-      #     api_key: @api_key }
-      # }.to_json
       
-      # gsm = gsm_cells || { gsm_cells: {
-      #     countrycode: 250,
-      #     operatorid: 99,
-      #     cellid: 42332,
-      #     lac: 36002,
-      #     signal_strength: nil,
-      #     age: nil }
-      # }.to_json
-      
-      # wifi = wifi_networks || {
-      #     wifi_networks: {
-      #         mac: '00-1C-F0-E4-BB-F5',
-      #         signal_strength: nil,
-      #         age: nil }
-      # }.to_json
-      
-      
-      # ip = ip || { ip: {
-      #     address_v4: '178.247.233.32' }
-      # }.to_json
-      
+      # ----- Input
       common = CommonSerializer.new(1.0, @api_key).to_json
       gsm_cells << GsmCellSerializer.new(250, 99, 42332, 36002, nil, nil)
       gsm_cells << GsmCellSerializer.new(250, 99, 42332, 36002, nil, nil)
       wifi_networks << WifiNetworkSerializer.new('00-1C-F0-E4-BB-F5', nil, nil)
       ip = IpSerializer.new('178.247.233.32').to_json
+      # Input ----
       
-      gsm_json = gsm_cells.map{|s| s.to_json}
-      wifi_json = wifi_networks.map{|s| s.to_json}
-
+      # Format input ------
+      gsm_json = gsm_cells.map { |s| s.to_json }
+      wifi_json = wifi_networks.map { |s| s.to_json }
       request = RequestSerializer.new(common, wifi_json, gsm_json, ip).to_json
-
       json = "json=#{request}".delete '\\'
-      puts json
-
-      url = 'http://api.lbs.yandex.net/geolocation'
-      response = HTTParty.post(url,
+      json.gsub!(/"{/, '{')
+      json.gsub!(/}"/, '}')
+      # ------- Format input
+      
+      response = HTTParty.post('http://api.lbs.yandex.net/geolocation',
                                { body: json,
                                  headers: {
                                      :'Content-Type' => 'application/x-www-form-urlencoded' }
                                }
       )
-
-      JSON.parse(response.body)
+      
+      puts JSON.parse(response.body)
     end
   end
 end
--- lib/run.rb
+++ lib/run.rb
@@ -1,5 +1,4 @@
 require_relative 'geolocator'
-require 'pp'
 
 client = Geolocator::Client.new('ADV6fFkBAAAAclZ3VAMBWcneV6qzQ4LK2EVFLw6LFO_ITtMAAAAAAAAAAAA18gaUA7A5aVVfG4Wn7S4D0hQGAQ==')
 client.perform_request
