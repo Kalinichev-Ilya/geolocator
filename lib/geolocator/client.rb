5dda893f442d6f2eb358fed3dccb2adc747667df
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-08-06 23:46:03 +0300

    final refactor

--- lib/geolocator/client.rb
+++ lib/geolocator/client.rb
@@ -8,38 +8,32 @@ require_relative 'helpers/JSONator'
 # client
 module Geolocator
   class Client
+    attr_reader :api_key
+    
     def initialize(api_key = nil)
       @api_key = api_key || ENV['YANDEX_ACCESS_TOKEN']
     end
     
-    def perform_request(common = nil, gsm_cells = [] || nil, wifi_networks = [] || nil, ip = nil)
+    def perform_request(common = nil, gsm_cells = nil, wifi_networks = nil, ip = nil)
       
-      # ----- Input
-      common = CommonSerializer.new(1.0, @api_key).to_json
-      gsm_cells << GsmCellSerializer.new(250, 99, 42332, 36002, nil, nil)
-      gsm_cells << GsmCellSerializer.new(250, 99, 42332, 36002, nil, nil)
-      wifi_networks << WifiNetworkSerializer.new('00-1C-F0-E4-BB-F5', nil, nil)
-      ip = IpSerializer.new('178.247.233.32').to_json
-      # Input ----
-      
-      # Format input ------
-      gsm_json = gsm_cells.map { |s| s.to_json }
-      wifi_json = wifi_networks.map { |s| s.to_json }
-      request = RequestSerializer.new(common, wifi_json, gsm_json, ip).to_json
-      json = "json=#{request}".delete '\\'
-      json.gsub!(/"{/, '{')
-      json.gsub!(/}"/, '}')
-      # ------- Format input
-      
+      json = "json=#{request_builder!(common, wifi_networks, gsm_cells, ip)}"
       response = HTTParty.post('http://api.lbs.yandex.net/geolocation',
-                               { body: json,
-                                 headers: {
-                                     :'Content-Type' => 'application/x-www-form-urlencoded' }
+                               {
+                                   body: json
                                }
       )
       
       puts JSON.parse(response.body)
     end
+    
+    private
+    
+    def request_builder!(common, wifi_networks, gsm_cells, ip)
+      json = RequestSerializer.new(common, wifi_networks, gsm_cells, ip).to_json.delete '\\'
+      json.gsub!(/"{/, '{')
+      json.gsub!(/}"/, '}')
+    end
+  
   end
 end
 
--- lib/geolocator/serializers/request_serializer.rb
+++ lib/geolocator/serializers/request_serializer.rb
@@ -5,6 +5,9 @@ class RequestSerializer
   attr_accessor :common, :wifi_networks, :gsm_cells, :ip
   
   def initialize(common, wifi_networks, gsm_cells, ip)
+    gsm_cells = gsm_cells.map { |s| s.to_json }
+    wifi_networks = wifi_networks.map { |s| s.to_json }
+    
     @common = common
     @wifi_networks = wifi_networks
     @gsm_cells = gsm_cells
